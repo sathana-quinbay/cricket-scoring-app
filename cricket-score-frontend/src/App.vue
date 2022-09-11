005be4362282dc87c59c9dbec85c10809c9e7daf
Sathana-V <sathanav.19cse@kongu.edu>
2022-09-11 17:56:16 +0530

    initialization

--- cricket-score-frontend/src/App.vue
+++ cricket-score-frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4>hello</h4>
+   <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
--- cricket-score-frontend/src/route/index.js
+++ cricket-score-frontend/src/route/index.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HelloWorld from "../components/HelloWorld";
+import WagonWheelComponent from '../components/WagonWheelComponent'
 Vue.use(VueRouter);
-const router = [
-    {path:'/home',name:'HelloWorld',component:HelloWorld}
+const routes = [
+    {path:'/',name:'HelloWorld',component:HelloWorld},
+    {path:'/wagon',name:'WagonWheelComponent',component:WagonWheelComponent}
 ]
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+  });
 export default router;
