9175688357798f106c2f894e433590efa95e2785
Sathana-V <sathanav.19cse@kongu.edu>
2022-09-11 17:59:16 +0530

    initialization

--- cricket-score-frontend/src/route/index.js
+++ cricket-score-frontend/src/route/index.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HelloWorld from "../components/HelloWorld";
-import WagonWheelComponent from '../components/WagonWheelComponent'
 Vue.use(VueRouter);
 const routes = [
     {path:'/',name:'HelloWorld',component:HelloWorld},
-    {path:'/wagon',name:'WagonWheelComponent',component:WagonWheelComponent}
+  
 ]
 const router = new VueRouter({
     mode: "history",
