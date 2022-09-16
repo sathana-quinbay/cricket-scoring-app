47120fa87a22494fe7aeb5923283104d73f879d1
KishoreMSK <mkishore2610@gmail.com>
2022-09-16 12:15:25 +0530

    day 5

--- cricket-score-frontend/src/Store/match.store.js
+++ cricket-score-frontend/src/Store/match.store.js
@@ -1,99 +1,69 @@
 import {getAllMatches} from "../Service/match.service"
 export default{
     state:{
-        matchList:[
-        //     {
-        //     chooseTo : null,
-        //     matchDate : "2022-09-23",
-        //     matchId:"37",
-        //     matchLocation:"hjk",
-        //     matchName:"vhbjnkm vs fghjksd",      
-        //     matchStatus:null,
-        //     matchTime:"06:44",
-        //     overs:10,
-        //     team1:null,
-        //     team1Id:70,
-        //     team2:null,
-        //     team2Id:72,
-        //     tossWonBy:"sda",
-        //     userid:57,
-        // },
-        // {
-        //     chooseTo : null,
-        //     matchDate : "2022-09-23",
-        //     matchId:"37",
-        //     matchLocation:"hjk",
-        //     matchName:"vhbjnkm vs fghjksd",      
-        //     matchStatus:null,
-        //     matchTime:"06:44",
-        //     overs:10,
-        //     team1:null,
-        //     team1Id:70,
-        //     team2:null,
-        //     team2Id:72,
-        //     tossWonBy:"sda",
-        //     userid:57,
-        // },
-        // {
-        //     chooseTo : null,
-        //     matchDate : "2022-09-23",
-        //     matchId:"37",
-        //     matchLocation:"hjk",
-        //     matchName:"vhbjnkm vs fghjksd",      
-        //     matchStatus:null,
-        //     matchTime:"06:44",
-        //     overs:10,
-        //     team1:null,
-        //     team1Id:70,
-        //     team2:null,
-        //     team2Id:72,
-        //     tossWonBy:"sda",
-        //     userid:57,
-        // },
-        // {
-        //     chooseTo : null,
-        //     matchDate : "2022-09-23",
-        //     matchId:"37",
-        //     matchLocation:"hjk",
-        //     matchName:"vhbjnkm vs fghjksd",      
-        //     matchStatus:null,
-        //     matchTime:"06:44",
-        //     overs:10,
-        //     team1:null,
-        //     team1Id:70,
-        //     team2:null,
-        //     team2Id:72,
-        //     tossWonBy:"sda",
-        //     userid:57,
-        // },
-        // {
-        //     chooseTo : null,
-        //     matchDate : "2022-09-23",
-        //     matchId:"37",
-        //     matchLocation:"hjk",
-        //     matchName:"vhbjnkm vs fghjksd",      
-        //     matchStatus:null,
-        //     matchTime:"06:44",
-        //     overs:10,
-        //     team1:null,
-        //     team1Id:70,
-        //     team2:null,
-        //     team2Id:72,
-        //     tossWonBy:"sda",
-        //     userid:57,
-        // },
-        ]
+        matchList:[],
+        teamMatch:[{
+            matchId: 57,
+            userid: 57, 
+            team1Id: 70, 
+            team2Id: 72, 
+            tossWonBy: 70, 
+            matchName: "CSK vs RCB", 
+            chooseTo: "Bat", 
+            matchLocation: "Bangalore", 
+            matchDate: "2022-09-23", 
+            matchTime: "06:44", 
+            matchStatus: null,
+            overs: 10, 
+        },
+        {
+            matchId: 57,
+            userid: 57, 
+            team1Id: 70, 
+            team2Id: 72, 
+            tossWonBy: 70, 
+            matchName: "vhbjnkm vs fghjksd", 
+            chooseTo: "Bat", 
+            matchLocation: "Bangalore", 
+            matchDate: "2022-09-23", 
+            matchTime: "06:44", 
+            matchStatus: null,
+            overs: 10, 
+        }],
+        
+            // team1: { 
+            //     teamid: 70, 
+            //     teamname: "vhbjnkm", 
+            //     teamlocation: "ghjkl", 
+            //     playerList: [ 
+            //         { playerid: 157, 
+            //         playername: "Ishan Kishan" }, 
+            //         { playerid: 158, 
+            //           playername: "Rohit Sharma" }, 
+            //           { playerid: 159, 
+            //             playername: "Surya Kumar Yadav" }, 
+            //             { playerid: 160, 
+            //                 playername: "Quinton De Kock" }, 
+            //                 { "playerid": 161, "playername": "Hardik Pandya" }, { "playerid": 162, "playername": "Krunal Pandya" }, { "playerid": 163, "playername": "Keiron Pollard" }, { "playerid": 164, "playername": "Jasprit Bumrah" }, { "playerid": 165, "playername": "Lasith Malinga" }, { "playerid": 166, "playername": "Trent Bolt" }, { "playerid": 167, "playername": "Jofra Archer" } ], "teamstatus": "removed" }, "team2": { "teamid": 72, "teamname": "fghjksd", "teamlocation": "cghvjbknml", "playerList": [ { "playerid": 146, "playername": "Ruturaj Gaikwad" }, { "playerid": 147, "playername": "Faf Du Plesis" }, { "playerid": 148, "playername": "Suresh Raina" }, { "playerid": 149, "playername": "Ambati rayudu" }, { "playerid": 150, "playername": "Robin Uthappa" }, { "playerid": 151, "playername": "Ravindra Jadeja" }, { "playerid": 152, "playername": "M S Dhoni" }, { "playerid": 153, "playername": "Dwane Bravo" }, { "playerid": 154, "playername": "Deepak Chahar" }, { "playerid": 155, "playername": "Shardhul Thakur" }, { "playerid": 156, "playername": "Moin Ali" } ], "teamstatus": "removed"
+            
+
+        
     },
     getters:{
         getMatchList(state)
         {
             return state.matchList;
+        },
+        getTeamMatches(state){
+            return state.teamMatch
         }
     },
     mutations:{
-        setMatch(state,value)
-        {
+        setMatch(state,value){
             state.matchList=value
+        },
+        setTeamMatches(state,value){
+            state.teamMatch = value
         }
     },
     actions:{
@@ -111,7 +81,9 @@ export default{
                 }
             }
             )
-        }
+        },
+      
+    }
 
     }
-}
+// }
---
+++ cricket-score-frontend/src/components/BallsComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+    <div class="display-match">
+    <b-card v-for="(key,index) in teamMatches" :key="index" class="match-card" bg-variant="dark" text-variant="white" title="">
+       <p>{{key.matchName}} </p> 
+  <b-card-text class="card-text">
+    {{key.matchLocation}}<br>
+    {{new Date(key.matchDate).toDateString()}}<br>
+     Time : {{key.matchTime}}<br>
+     Choose to : {{key.chooseTo}}
+  </b-card-text>
+</b-card>
+</div>
+  </div>
+  
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    computed:{
+        ...mapGetters({
+            // teamMatches:"getTeamMatches"
+        })
+    },
+    mounted(){
+        console.log(this.teamMatches);
+    }
+}
+</script>
+
+<style scoped>
+.display-match{
+    width: 60%;
+    height: 25%;
+    margin: 0% 20%;
+    font-size: 25px;
+}
+.match-card{
+    border-radius: 20px;
+    margin-top: 5%;
+   
+}
+.card-text{
+    /* font-size: 25px; */
+}
+</style>
